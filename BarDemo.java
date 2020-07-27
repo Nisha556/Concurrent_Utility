d18ea92f3da807d1841042765b0233ea4b72f248
Nisha556 <shandilyanisha1@gmail.com>
2020-07-27 22:26:30 +0530

    Concurrent prog

---
+++ BarDemo.java
@@ -0,0 +1,38 @@
+package Concurrent_Utility;
+
+
+import java.util.concurrent.*;
+class BarDemo {
+public static void main(String args[]) {
+CyclicBarrier cb = new CyclicBarrier(3, new BarAction() );
+System.out.println("Starting");
+new MyThread1(cb, "A");
+new MyThread1(cb, "B");
+new MyThread1(cb, "C");
+}
+}
+class MyThread1 implements Runnable {
+CyclicBarrier cbar;
+String name;
+MyThread1(CyclicBarrier c, String n) {
+cbar = c;
+name = n;
+new Thread(this).start();
+}
+public void run() {
+System.out.println(name);
+try {
+cbar.await();
+} catch (BrokenBarrierException exc) {
+System.out.println(exc);
+} catch (InterruptedException exc) {
+System.out.println(exc);
+}
+}
+}
+// An object of this class is ca
+class BarAction implements Runnable {
+public void run() {
+System.out.println("Barrier Reached!");
+}
+}
